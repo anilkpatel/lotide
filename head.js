312531157bdaeb6257c2c4be8a800bfd8154f045
anilkpatel <anil.patel@mail.mcgill.ca>
2021-01-12 03:48:28 +0000

    Incorporated Mocha and Chai.js

--- head.js
+++ head.js
@@ -1,6 +1,25 @@
-//Week 01, Day 01
-//Implement head
-//Lotide Challenge
+//Week 01, Day 03 and W01, Day 02 
+//Implement head  
+//Lotide Challenge with Refactoring 
+//Anil Patel
+
+//const assertEqual = require('../assertEqual'); //Do not need this  
+
+//FUNCTION: Tail 
+
+function head(array) {
+  //const number = array[0]; //return the element itself, not as array
+  return array[0]
+}
+
+//module.exports = assertEqual; // don't export 
+module.exports = head; //export code from this module //CHECK
+
+//const assertEqual = function(actual, expected) { ... };
+
+
+
+//DEPRECATED CODE W01 Day 03 
 
 /* INSTRUCTIONS:
 
@@ -18,11 +37,6 @@ You should always be thinking about other scenarios to consider. Here are some s
     An empty array should yield undefined as its head
 */
 
-function head(array) {
-  //const number = array[0]; //return the element itself, not as array
-  return array[0]
-}
-
 //These two could work
 /*
 function head(array) {
@@ -35,7 +49,7 @@ const tail = function(array) {
   return newArray;  //slice good for remove 1st few elements of array
 };
 */
-
+/*
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`Assertion Passed ❤: ${actual} === ${expected}`)
@@ -45,11 +59,13 @@ const assertEqual = function(actual, expected) {
     return false;
   } 
 };
+*/
 
+/*
 // TEST CODE
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual("Bootcampp", "Bootcamp");
 assertEqual(6, 6);
-
+*/
